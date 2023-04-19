38cc53c119462b8c3c9975aac620244fe2221f5b
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-04-19 22:54:57 +1000

    feat: Result with ?.value|error properties

--- src/result.d.ts
+++ src/result.d.ts
@@ -1,21 +1,45 @@
 import type { Enum } from "./enum";
 
 /**
- * Represents either success (`Ok`) or failure (`Err`).
- *
- * @example
- * ```
- * Result<number>
- * -> | { is: "Ok", value: number }
- *    | { is: "Err", error: unknown }
- *
- * Result<number, "FetchError">
- * -> | { is: "Ok", value: number }
- *    | { is: "Err", error: "FetchError" }
- * ```
+Represents either success (`Ok`) or failure (`Err`).
+
+_`Result` uses `value?: never` and `error?: never` to allow for shorthand access
+to `.value` or `.error` if you want to safely default to `undefined` if either
+property is not present._
+
+```ts
+Result<number>
+-> | { is: "Ok"; value: number; error?: never }
+   | { is: "Err"; error: unknown; value?: never }
+
+Result<number, "FetchError">
+-> | { is: "Ok"; value: number; error?: never }
+   | { is: "Err"; error: "FetchError"; value?: never }
+```
+
+```ts
+const getUser = async (name: string): Promise<Result<User, "NotFound">> => {
+	return { is: "Ok", value: user };
+	return { is: "Err", error: "NotFound" };
+}
+
+const $user = await getUser("foo");
+if ($user.is === "Err") { return ... }
+const user = $user.value;
+
+const $user = await getUser("foo");
+const userOrUndefined = $user.value;
+const userOrUndefined = $user.is === "Ok" ? $user.value : undefined;
+
+const $user = await getUser("foo");
+const userOrDefault = $user.value ?? defaultUser;
+const userOrDefault = $user.is === "Ok" ? $user.value : defaultUser;
+```
+
+Based on Rust's
+[`Result`](https://doc.rust-lang.org/std/result/enum.Result.html) enum.
  */
-// https://doc.rust-lang.org/std/result/enum.Result.html
 export type Result<T = unknown, E = unknown> = Enum<{
-	Ok: { value: T };
-	Err: { error: E };
+	Ok: { value: T; error?: never };
+	Err: { error: E; value?: never };
 }>;
--- src/result.typespec.ts
+++ src/result.typespec.ts
@@ -3,30 +3,38 @@ import type { Result } from "./result";
 
 ({}) as [
 	Expect<
-		Equal<Result, { is: "Ok"; value: unknown } | { is: "Err"; error: unknown }>
+		Equal<
+			Result,
+			| { is: "Ok"; value: unknown; error?: never }
+			| { is: "Err"; error: unknown; value?: never }
+		>
 	>,
 	Expect<
 		Equal<
 			Result<never>,
-			{ is: "Ok"; value: never } | { is: "Err"; error: unknown }
+			| { is: "Ok"; value: never; error?: never }
+			| { is: "Err"; error: unknown; value?: never }
 		>
 	>,
 	Expect<
 		Equal<
 			Result<unknown>,
-			{ is: "Ok"; value: unknown } | { is: "Err"; error: unknown }
+			| { is: "Ok"; value: unknown; error?: never }
+			| { is: "Err"; error: unknown; value?: never }
 		>
 	>,
 	Expect<
 		Equal<
 			Result<never, never>,
-			{ is: "Ok"; value: never } | { is: "Err"; error: never }
+			| { is: "Ok"; value: never; error?: never }
+			| { is: "Err"; error: never; value?: never }
 		>
 	>,
 	Expect<
 		Equal<
 			Result<unknown, unknown>,
-			{ is: "Ok"; value: unknown } | { is: "Err"; error: unknown }
+			| { is: "Ok"; value: unknown; error?: never }
+			| { is: "Err"; error: unknown; value?: never }
 		>
 	>
 ];
