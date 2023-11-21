ed20603120b185d4e671cce6d4c02336b44a881c
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-11-21 21:36:20 +1100

    wip: working match with inline fallback matcher

--- src/enum/match.ts
+++ src/enum/match.ts
@@ -3,39 +3,59 @@
 import type { DiscriminantAny, DiscriminantDefault } from "./enum";
 import type { Infer } from "./infer";
 
-export const match = <
-	TEnum extends Record<TDiscriminant, DiscriminantAny>,
-	TMatcher extends {
-		[Key in keyof Infer<TEnum>]?: (value: Infer<TEnum>[Key]) => unknown;
-	},
-	TDefault extends (() => any) | (() => never) = () => never,
-	TDiscriminant extends DiscriminantAny = DiscriminantDefault
->(
-	value: TEnum | [TEnum, TDiscriminant],
-	matcher: (IsNever<ReturnType<TDefault>> extends true
-		? Required<TMatcher>
-		: Partial<TMatcher>) & {
-		_?: TDefault;
-	}
-) => {
-	const [valueEnum, valueDiscriminant] = Array.isArray(value)
-		? value
-		: [value, "_type" as TDiscriminant];
+// ---
 
-	const key = valueEnum[valueDiscriminant];
-	const keyMatchFn =
-		matcher[key as unknown as keyof TMatcher & string] ??
-		matcher["_" as keyof TMatcher & string];
-	if (!keyMatchFn) {
-		throw TypeError(`unhandled enum variant: ${JSON.stringify(value)}`);
-	}
-	return keyMatchFn(valueEnum as any) as
-		| {
-				[Key in keyof TMatcher]: TMatcher[Key] extends (...args: any[]) => any
-					? ReturnType<TMatcher[Key]>
-					: never;
-		  }[keyof TMatcher]
-		| (TDefault extends (...args: any[]) => any ? ReturnType<TDefault> : never);
-};
+type EnumAny<TDiscriminant extends DiscriminantAny> = Record<
+	TDiscriminant,
+	string
+>;
 
-type IsNever<T> = [T] extends [never] ? true : false;
+export const match =
+	<
+		TEnum extends EnumAny<TDiscriminant>,
+		TDiscriminant extends keyof TEnum & string = keyof TEnum &
+			DiscriminantDefault
+	>(
+		value: TEnum,
+		discriminant: TDiscriminant = "_type" as TDiscriminant
+	) =>
+	<
+		TEnumMatcher extends {
+			[Key in keyof Infer<TEnum, TDiscriminant>]: Infer<
+				TEnum,
+				TDiscriminant
+			>[Key] extends true
+				? () => unknown
+				: (value: Infer<TEnum, TDiscriminant>[Key]) => unknown;
+		},
+		TMatcher extends TFallback extends () => unknown
+			? [ReturnType<TFallback>] extends [never]
+				? TEnumMatcher
+				: Partial<TEnumMatcher>
+			: TEnumMatcher,
+		TFallback extends (() => unknown) | (() => never) | undefined = () => never
+	>(
+		matcher: TMatcher & { _?: TFallback }
+	): {
+		_: {
+			[Key in keyof TMatcher]: TMatcher[Key] extends (...args: any[]) => any
+				? ReturnType<TMatcher[Key]>
+				: never;
+		}[keyof TMatcher];
+		$: {
+			matcher: TMatcher;
+			fallback: TFallback;
+			keyofTEnum: keyof Infer<TEnum, TDiscriminant>;
+			keyofTEnumMatcher: keyof TEnumMatcher;
+			keyofTMatcher: keyof TMatcher;
+		};
+	}["_"] => {
+		const key = value[discriminant];
+		const keyMatchFn =
+			matcher[key as unknown as keyof TMatcher & string] ??
+			matcher["_" as keyof TMatcher & string];
+		if (!keyMatchFn) {
+			throw TypeError(`unhandled enum variant: ${JSON.stringify(value)}`);
+		}
+		return keyMatchFn(value as any) as any;
+	};
