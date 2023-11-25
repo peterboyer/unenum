96273bee40e7aa23ad2cb941f61530f002b36563
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-11-25 15:01:50 +1100

    feat: simplify match fn call

--- src/match.spec.ts
+++ src/match.spec.ts
@@ -16,7 +16,7 @@ describe("match", () => {
 			// all cases
 			{
 				const fn = (value: Value) =>
-					match(value)({
+					match(value, {
 						One: () => "One" as const,
 					});
 				!0 as Expect<Equal<ReturnType<typeof fn>, "One">>;
@@ -25,7 +25,7 @@ describe("match", () => {
 			// only fallback
 			{
 				const fn = (value: Value) =>
-					match(value)({
+					match(value, {
 						_: () => "Fallback" as const,
 					});
 				!0 as Expect<Equal<ReturnType<typeof fn>, "Fallback">>;
@@ -34,7 +34,7 @@ describe("match", () => {
 			// all cases and forbid fallback
 			{
 				const fn = (value: Value) =>
-					match(value)({
+					match(value, {
 						One: () => "One" as const,
 						_: undefined,
 					});
@@ -57,7 +57,7 @@ describe("match", () => {
 			// all cases
 			{
 				const fn = (value: Value) =>
-					match(value)({
+					match(value, {
 						One: () => "One" as const,
 						Two: () => "Two" as const,
 					});
@@ -67,7 +67,7 @@ describe("match", () => {
 			// only fallback
 			{
 				const fn = (value: Value) =>
-					match(value)({
+					match(value, {
 						_: () => "Fallback" as const,
 					});
 				!0 as Expect<Equal<ReturnType<typeof fn>, "Fallback">>;
@@ -76,7 +76,7 @@ describe("match", () => {
 			// one case and fallback
 			{
 				const fn = (value: Value) =>
-					match(value)({
+					match(value, {
 						One: () => "One" as const,
 						_: () => "Fallback" as const,
 					});
@@ -86,7 +86,7 @@ describe("match", () => {
 			// all cases and forbid fallback
 			{
 				const fn = (value: Value) =>
-					match(value)({
+					match(value, {
 						One: () => "One" as const,
 						Two: () => "Two" as const,
 						_: undefined,
@@ -97,7 +97,7 @@ describe("match", () => {
 			// all cases, some using value properties
 			{
 				const fn = (value: Value) =>
-					match(value)({
+					match(value, {
 						One: () => 0,
 						Two: ({ value }) => value,
 						_: () => "Unknown",
@@ -112,7 +112,7 @@ describe("match", () => {
 			// all cases
 			{
 				const fn = (value: Value) =>
-					match(value)({
+					match(value, {
 						One: () => "One" as const,
 						Two: () => "Two" as const,
 						_: undefined,
@@ -124,7 +124,7 @@ describe("match", () => {
 
 			{
 				const fn = (value: Value) =>
-					match(value)({
+					match(value, {
 						_: () => "Unknown",
 					});
 
@@ -135,7 +135,7 @@ describe("match", () => {
 
 			{
 				const fn = (value: Value) =>
-					match(value)({
+					match(value, {
 						One: () => 0,
 						Two: ({ value }) => value,
 						_: () => "Unknown",
@@ -143,6 +143,151 @@ describe("match", () => {
 
 				expect(fn({ _type: "One" })).toEqual(0);
 				expect(fn({ _type: "Two", value: "..." })).toEqual("...");
+				expect(fn({} as Value)).toEqual("Unknown");
+			}
+		});
+	});
+
+	describe("custom discriminant", () => {
+		test("value with one variant", () => {
+			type Value = Enum<{ One: true }, "custom">;
+
+			// @ts-expect-error Is missing the `One` or `_` matcher case.
+			void (() => match({} as Value)({}));
+
+			// @ts-expect-error Is missing the `One` matcher case.
+			void (() => match({} as Value)({ _: undefined }));
+
+			// all cases
+			{
+				const fn = (value: Value) =>
+					match(value, {
+						One: () => "One" as const,
+					});
+				!0 as Expect<Equal<ReturnType<typeof fn>, "One">>;
+			}
+
+			// only fallback
+			{
+				const fn = (value: Value) =>
+					match(value, {
+						_: () => "Fallback" as const,
+					});
+				!0 as Expect<Equal<ReturnType<typeof fn>, "Fallback">>;
+			}
+
+			// all cases and forbid fallback
+			{
+				const fn = (value: Value) =>
+					match(value, {
+						One: () => "One" as const,
+						_: undefined,
+					});
+				!0 as Expect<Equal<ReturnType<typeof fn>, "One">>;
+			}
+		});
+
+		test("value with many variants", () => {
+			type Value = Enum<{ One: true; Two: { value: string } }, "custom">;
+
+			// @ts-expect-error Is missing the `One` or `_` matcher case.
+			void (() => match([{} as Value, "custom"])({}));
+
+			// @ts-expect-error Is missing the `One` matcher case.
+			void (() => match([{} as Value, "custom"])({ _: undefined }));
+			//
+			// @ts-expect-error Is missing the `Two` matcher case.
+			void (() => match([{} as Value, "custom"])({ One: () => true }));
+
+			// all cases
+			{
+				const fn = (value: Value) =>
+					match([value, "custom"], {
+						One: () => "One" as const,
+						Two: () => "Two" as const,
+					});
+				!0 as Expect<Equal<ReturnType<typeof fn>, "One" | "Two">>;
+			}
+
+			// only fallback
+			{
+				const fn = (value: Value) =>
+					match([value, "custom"], {
+						_: () => "Fallback" as const,
+					});
+				!0 as Expect<Equal<ReturnType<typeof fn>, "Fallback">>;
+			}
+
+			// one case and fallback
+			{
+				const fn = (value: Value) =>
+					match([value, "custom"], {
+						One: () => "One" as const,
+						_: () => "Fallback" as const,
+					});
+				!0 as Expect<Equal<ReturnType<typeof fn>, "One" | "Fallback">>;
+			}
+
+			// all cases and forbid fallback
+			{
+				const fn = (value: Value) =>
+					match([value, "custom"], {
+						One: () => "One" as const,
+						Two: () => "Two" as const,
+						_: undefined,
+					});
+				!0 as Expect<Equal<ReturnType<typeof fn>, "One" | "Two">>;
+			}
+
+			// all cases, some using value properties
+			{
+				const fn = (value: Value) =>
+					match([value, "custom"], {
+						One: () => 0,
+						Two: ({ value }) => value,
+						_: () => "Unknown",
+					});
+				!0 as Expect<Equal<ReturnType<typeof fn>, number | string>>;
+			}
+		});
+
+		test("runtime", () => {
+			type Value = Enum<{ One: true; Two: { value: string } }, "custom">;
+
+			// all cases
+			{
+				const fn = (value: Value) =>
+					match([value, "custom"], {
+						One: () => "One" as const,
+						Two: () => "Two" as const,
+						_: undefined,
+					});
+				expect(fn({ custom: "One" })).toEqual("One");
+				expect(fn({ custom: "Two", value: "..." })).toEqual("Two");
+				expect(() => fn({} as Value)).toThrow();
+			}
+
+			{
+				const fn = (value: Value) =>
+					match([value, "custom"], {
+						_: () => "Unknown",
+					});
+
+				expect(fn({ custom: "One" })).toEqual("Unknown");
+				expect(fn({ custom: "Two", value: "Two" })).toEqual("Unknown");
+				expect(fn({} as Value)).toEqual("Unknown");
+			}
+
+			{
+				const fn = (value: Value) =>
+					match([value, "custom"], {
+						One: () => 0,
+						Two: ({ value }) => value,
+						_: () => "Unknown",
+					});
+
+				expect(fn({ custom: "One" })).toEqual(0);
+				expect(fn({ custom: "Two", value: "..." })).toEqual("...");
 				expect(fn({} as Value)).toEqual("Unknown");
 			}
 		});
--- src/match.ts
+++ src/match.ts
@@ -2,62 +2,60 @@
 
 import type { Enum } from "./enum";
 
-export const match =
-	<
-		TEnum extends Enum.Any<TDiscriminant>,
-		TDiscriminant extends keyof TEnum & string = keyof TEnum &
-			Enum.Discriminant.Default
-	>(
-		value: TEnum,
-		discriminant: TDiscriminant = "_type" as TDiscriminant
-	) =>
-	<
-		TEnumMatcher extends {
-			[Key in keyof Enum.Infer<TEnum, TDiscriminant>]: Enum.Infer<
-				TEnum,
-				TDiscriminant
-			>[Key] extends true
-				? () => unknown
-				: (value: Enum.Infer<TEnum, TDiscriminant>[Key]) => unknown;
-		},
-		TMatcher extends TFallback extends () => unknown
-			? [ReturnType<TFallback>] extends [never]
-				? TEnumMatcher
-				: Partial<TEnumMatcher>
-			: TEnumMatcher,
-		TFallback extends (() => unknown) | (() => never) | undefined = () => never
-	>(
-		matcher: TMatcher & { _?: TFallback }
-	): {
-		_:
-			| (undefined extends TMatcher[Exclude<keyof TMatcher, "_">]
-					? never
-					: {
-							[Key in keyof TMatcher]: TMatcher[Key] extends (
-								...args: any[]
-							) => any
-								? ReturnType<TMatcher[Key]>
-								: never;
-					  }[keyof TMatcher])
-			| (TFallback extends (...args: any[]) => any
-					? ReturnType<TFallback>
-					: never);
-		// debug
-		$: {
-			matcher: TMatcher;
-			fallback: TFallback;
-			discriminant: TDiscriminant;
-			keyofTEnum: keyof TEnum;
-			keyofTEnumMatcher: keyof TEnumMatcher;
-			keyofTMatcher: keyof TMatcher;
-		};
-	}["_"] => {
-		const key = value[discriminant];
-		const keyMatchFn =
-			matcher[key as unknown as keyof TMatcher & string] ??
-			matcher["_" as keyof TMatcher & string];
-		if (!keyMatchFn) {
-			throw TypeError(`unhandled enum variant: ${JSON.stringify(value)}`);
-		}
-		return keyMatchFn(value as any) as any;
+export const match = <
+	TEnum extends Enum.Any<TDiscriminant>,
+	TEnumMatcher extends {
+		[Key in keyof Enum.Infer<TEnum, TDiscriminant>]: Enum.Infer<
+			TEnum,
+			TDiscriminant
+		>[Key] extends true
+			? () => unknown
+			: (value: Enum.Infer<TEnum, TDiscriminant>[Key]) => unknown;
+	},
+	TMatcher extends TFallback extends () => unknown
+		? [ReturnType<TFallback>] extends [never]
+			? TEnumMatcher
+			: Partial<TEnumMatcher>
+		: TEnumMatcher,
+	TFallback extends (() => unknown) | (() => never) | undefined = () => never,
+	TDiscriminant extends keyof TEnum & string = keyof TEnum &
+		Enum.Discriminant.Default
+>(
+	target: TEnum | [value: TEnum, discriminant: TDiscriminant],
+	matcher: TMatcher & { _?: TFallback }
+): {
+	_:
+		| (undefined extends TMatcher[Exclude<keyof TMatcher, "_">]
+				? never
+				: {
+						[Key in keyof TMatcher]: TMatcher[Key] extends (
+							...args: any[]
+						) => any
+							? ReturnType<TMatcher[Key]>
+							: never;
+				  }[keyof TMatcher])
+		| (TFallback extends (...args: any[]) => any
+				? ReturnType<TFallback>
+				: never);
+	// debug
+	$: {
+		matcher: TMatcher;
+		fallback: TFallback;
+		discriminant: TDiscriminant;
+		keyofTEnum: keyof TEnum;
+		keyofTEnumMatcher: keyof TEnumMatcher;
+		keyofTMatcher: keyof TMatcher;
 	};
+}["_"] => {
+	const [value, discriminant] = Array.isArray(target)
+		? target
+		: [target, "_type" as TDiscriminant];
+	const key = value[discriminant];
+	const keyMatchFn =
+		matcher[key as unknown as keyof TMatcher & string] ??
+		matcher["_" as keyof TMatcher & string];
+	if (!keyMatchFn) {
+		throw TypeError(`unhandled enum variant: ${JSON.stringify(value)}`);
+	}
+	return keyMatchFn(value as any) as any;
+};
