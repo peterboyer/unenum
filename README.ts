0a101b1f5aed1a3d984d352ac202dca656cd0353
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2023-12-13 00:22:58 +1100

    feat!: match args to match Enum.is discriminator

--- README.ts
+++ README.ts
@@ -158,7 +158,7 @@ void ((file: File): string => {
 
 // (b) match expression
 void ((file: File): string =>
-	match([file, "mime"], {
+	match(file, "mime", {
 		"text/plain": () => "Text",
 		"image/jpeg": () => "Image",
 		_: () => "Unsupported",
