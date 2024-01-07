fde14104510cebcf1c011e7ad18c7d42558c7c08
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-01-07 21:55:56 +1100

    feat: remove ambient/global

--- src/global.async.ts
+++
@@ -1,6 +0,0 @@
-import type { Async as _Async } from "./async.js";
-
-declare global {
-	// @ts-expect-error Duplicates don't exist for consumer.
-	export { _Async as Async };
-}
--- src/global.result.ts
+++
@@ -1,6 +0,0 @@
-import type { Result as _Result } from "./result.js";
-
-declare global {
-	// @ts-expect-error Duplicates don't exist for consumer.
-	export { _Result as Result };
-}
