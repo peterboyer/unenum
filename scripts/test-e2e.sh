8a72c6ab4d7cc5304095b23a4a266324a5245e04
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-01-07 21:55:56 +1100

    feat: remove ambient/global

--- scripts/test-e2e.sh
+++ scripts/test-e2e.sh
@@ -2,23 +2,8 @@
 
 cd test
 
-# setup
 yarn
 rm -r node_modules/unenum
 (cd node_modules && ln -s ../../dist unenum)
 
-# libraries
 yarn tsc --project tsconfig.json
-
-cp index.ts index.global.ts
-sed -i'' 's/^import type .*/import "unenum\/global";/g' index.global.ts
-cp tsconfig.json tsconfig.global.json
-sed -i'' 's/index.ts/index.global.ts/g' tsconfig.global.json
-
-# applications
-yarn tsc --project tsconfig.global.json
-
-# cleanup
-rm \
-	index.global.ts \
-	tsconfig.global.json
--- src/global.enum.ts
+++
@@ -1,6 +0,0 @@
-import type { Enum as _Enum } from "./enum.js";
-
-declare global {
-	// @ts-expect-error Duplicates don't exist for consumer.
-	export { _Enum as Enum };
-}
