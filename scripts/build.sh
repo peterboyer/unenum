f8da83c7f5cd83eb81f7e6654377f6ccdd3fff7e
Peter Boyer <8391902+peterboyer@users.noreply.github.com>
2024-01-13 12:14:29 +1100

    feat: build script use npm pkg instead of dot-json

--- scripts/build.sh
+++ scripts/build.sh
@@ -8,12 +8,12 @@ mkdir -p dist
 
 # package
 cp package.json LICENSE README.md dist
-dot-json dist/package.json scripts --delete
-dot-json dist/package.json devDependencies --delete
+(cd dist && npm pkg delete scripts)
+(cd dist && npm pkg delete devDependencies)
 
-version_base=$(dot-json dist/package.json version)
+version_base=$(cd dist && npm pkg get version | tr -d '\"')
 version_complete=$(scripts/version.sh $version_base | awk '{print$1}')
 if [[ "$PUBLISH_CHANNEL" == "sha" ]]; then
 	version_complete="$version_complete-sha.$(git rev-parse --short HEAD)"
 fi
-dot-json dist/package.json version $version_complete
+(cd dist && npm pkg set version=$version_complete)
